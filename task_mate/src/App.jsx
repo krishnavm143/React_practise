c685c6588db6ec57288dd605c5eb957b9c228bb1
krishna <krishnavm143@gmail.com>
2023-09-01 08:32:54 +0530

    projects

---
+++ task_mate/src/App.jsx
@@ -0,0 +1,17 @@
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div>
+        <h3>Krishna</h3>
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ task_mate/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import logo from '../assets/logo.png'
+const Header = () => {
+    return (
+        <div>
+            <img className='logo' src={logo} alt="logo" />
+        </div>
+    )
+}
+
+export default Header
